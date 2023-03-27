f9a5a82e0bf446bb96f8202360e5e053d8ed8136
RicardoArquimedes <richardcqt28@gmail.com>
2023-03-26 20:47:03 -0500

    :art: updating sub logo asset

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-r.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoSubtitle from '../../assets/images/logo_sub_r.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 const Sidebar = () => {
