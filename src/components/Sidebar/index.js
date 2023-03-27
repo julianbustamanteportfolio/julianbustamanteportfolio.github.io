cbad455239117c1686041392d92c06838f9457c7
RicardoArquimedes <richardcqt28@gmail.com>
2023-03-26 19:41:37 -0500

    :construction: updating raect app and creating sidebar and navbar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import React from 'react' 
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-r.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+const Sidebar = () => {
+  return (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo'/>
+            <img className='sub-logo' src={LogoSubtitle} alt='Ricardo'/>
+        </Link>
+        <nav>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                 <FontAwesomeIcon icon={faHome} color='#4d4d4e'></FontAwesomeIcon>
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
+                 <FontAwesomeIcon icon={faUser} color='#4d4d4e'></FontAwesomeIcon>
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className='contact-link 'to='/contact'>
+                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'></FontAwesomeIcon>
+            </NavLink>
+        </nav>
+    </div>
+  )
+}
+
+export default Sidebar
