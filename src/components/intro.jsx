0bf507851f94262b8a57f057f9777aed12dc460f
RicardoArquimedes <richardcqt28@gmail.com>
2024-04-07 16:12:37 -0500

    setting colors for navbar and font styles

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -15,7 +15,7 @@ class Intro extends React.Component{
                   <h1 className="intro-title mb-4">Hola, soy Julián Bustamante</h1>
                   <p className="intro-subtitle"><span className="text-slider-items">
                     </span><strong className="text-slider"><Typed strings={['Paid Media Specialist', 'Traffiker', 'Planner Strategy', 'Media Buyer']} typeSpeed = {80} backDelay = {1100} backSpeed = {30} loop/></strong></p>
-                  <p className="pt-3"><a className="btn btn-primary btn js-scroll px-4" href="#about" role="button">View My Work</a></p>
+                  <p className="pt-3"><a className="btn btn-primary btn js-scroll px-4" href="#about" role="button">Portafolio</a></p>
                 </div>
               </div>
             </div>
