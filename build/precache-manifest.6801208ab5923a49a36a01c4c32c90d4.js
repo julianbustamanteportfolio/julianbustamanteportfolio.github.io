7d41b1feb373eba61bd6f4748d8548418bbc0fe4
RicardoArquimedes <richardcqt28@gmail.com>
2024-04-21 16:38:31 -0500

    :construction: updating portfolio section

---
+++ build/precache-manifest.6801208ab5923a49a36a01c4c32c90d4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c3837e99974d833a63146988d12589",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a05f3db730c7c8d6875",
+    "url": "/static/css/2.1275d5ef.chunk.css"
+  },
+  {
+    "revision": "efe6a5218a1050b7a625",
+    "url": "/static/css/main.a1d0a0e7.chunk.css"
+  },
+  {
+    "revision": "8a05f3db730c7c8d6875",
+    "url": "/static/js/2.58bb8896.chunk.js"
+  },
+  {
+    "revision": "efe6a5218a1050b7a625",
+    "url": "/static/js/main.32b8ff51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "866bf994deaaab5dca3febcc583b326e",
+    "url": "/static/media/background-banner.866bf994.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "eabbfa6787d3e2362f83a8bbb8d66d14",
+    "url": "/static/media/image-1.eabbfa67.png"
+  },
+  {
+    "revision": "7389c86b0b65cc7ae7ea79c426348a75",
+    "url": "/static/media/image-2.7389c86b.png"
+  },
+  {
+    "revision": "c81b1418146b30d47e2522219c3f5e5a",
+    "url": "/static/media/image-3.c81b1418.png"
+  },
+  {
+    "revision": "a24c86493f4fca118e23bb58d3686b2a",
+    "url": "/static/media/image-4.a24c8649.png"
+  },
+  {
+    "revision": "a0c1d5280a9d064d9e15b0c7f1ca014b",
+    "url": "/static/media/image-5.a0c1d528.png"
+  },
+  {
+    "revision": "b94afdbadcd1d009ab256e6e51cd01f9",
+    "url": "/static/media/image-6.b94afdba.png"
+  },
+  {
+    "revision": "385653d40135d08d31de747ec09a8ea8",
+    "url": "/static/media/image-7.385653d4.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "4e425755523cb88616411783f3f080b8",
+    "url": "/static/media/julianB.4e425755.jpg"
+  },
+  {
+    "revision": "3c37539e9ccb841d868039073e0ed785",
+    "url": "/static/media/video-1.3c37539e.mov"
+  },
+  {
+    "revision": "88a1274bbb99660568a1d53a391e92ba",
+    "url": "/static/media/video-2.88a1274b.mov"
+  }
+]);
